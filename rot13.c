e05b4dc967918dc53a0839ffd66156566da14c89
gdfurr98 <91289301+gdfurr98@users.noreply.github.com>
2024-10-08 19:42:10 -0700

    Update 1 rot13.c
    Added a test that kills the program if malloc fails. Added an option to change the ROT value and added logic to respond to change in ROT value dynamically. Added input validation to make sure that only numbers between 1 and 25 get entered by the user. Fixed a bug where the letters z and Z were not being ROTted. Removed the debugging print statements. Next update will take a string from the user, then you will enter your ROT value, and then it will be output to STDOUT.

--- rot13.c
+++ rot13.c
@@ -3,26 +3,36 @@
 #include <string.h>
 
 int main() {
-    char *initial_string = "They're trashing our rights man! Hack the planet!";
+    char *initial_string = "The quick brown fox jumps over the lazy dog.";
     size_t string_length = strlen(initial_string);
-    char *final_string = malloc(string_length * sizeof(char));
-    int position = 13;
+    char *final_string = malloc(string_length + 1 * sizeof(char));
+    if (final_string == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    int position;
+    int remainder;
+    printf("Enter a number between 1-25\n");
+    scanf("%d", &position);
+    while (position < 1 || position > 25){
+        printf("Invalid input. Please enter a number between 1 and 25.\n");
+        scanf("%d", &position);
+    }
     for (size_t i=0; i < string_length; i++) {
         int unicode_value = (int)initial_string[i];
-        if ((unicode_value >= 97) && (unicode_value < 122)){
-            printf("unicode value: %d\n", unicode_value);
+        if ((unicode_value >= 97) && (unicode_value <= 122)){
             if (unicode_value + position <= 122) {
                 unicode_value += position;
             } else {
-                unicode_value -= position;
+                remainder = 122 - unicode_value;
+                unicode_value = 96 + (position-remainder);
             }
-            printf("final string unicode value: %d\n", unicode_value);
-        } else if ((unicode_value >= 65) && (unicode_value < 90)){
-            printf("unicode value: %d\n", unicode_value);
+        } else if ((unicode_value >= 65) && (unicode_value <= 90)){
             if (unicode_value + position <= 90) {
                 unicode_value += position;
             } else {
-                unicode_value -= position;
+                remainder = 90 - unicode_value;
+                unicode_value = 64 + (position-remainder);
             }
         }
         final_string[i] = (char)unicode_value;
